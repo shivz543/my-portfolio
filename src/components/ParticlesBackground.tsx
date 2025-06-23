150a904098a9f0af0e7c77113a5a066928812f8d
shivz543 <shivangip.03r@gmail.com>
2025-06-23 13:09:37 +0530

    fix:loadfull

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -2,7 +2,7 @@
 
 import { useCallback } from 'react';
 import Particles from '@tsparticles/react';
-import { loadFull } from '@tsparticles/engine';
+import { loadFull } from 'tsparticles';
 import type { Engine } from '@tsparticles/engine';
 
 const ParticlesBackground = () => {
